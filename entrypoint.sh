7dd24c6ee5c367656653dd0af9248acecb85f7e0
Semenovsd <semenov_sd@bk.ru>
2020-10-04 02:53:49 +0300

    refact settings

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,6 +27,3 @@ mkdir "${SSL_DIR}"
 openssl req -newkey rsa:2048 -sha256 -nodes -keyout "${SSL_DIR}${SSL_PRIV}" -x509 -days 365 -out "${SSL_DIR}${SSL_CERT}" \
 -subj "/C=US/ST=Oregon/L=Portland/O=Company Name/OU=Org/CN=${DOMAIN_NAME_OR_IP}"
 sudo chmod -R +r "${SSL_DIR}"
-
-# Run docker-compose
-#docker-compose up --build
